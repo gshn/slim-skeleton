09ae61bd3cb1d0d06f5986ccafa01e361dc1e069
gshn <gshn@kakao.com>
2024-01-25 23:28:15 +0900

    feat: CommonData Class

---
+++ src/Domain/CommonData.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Domain;
+
+abstract class CommonData
+{
+    // 배열의 키가 클래스 맴버가 아닐 경우 필터링
+    public static function filterProperty(array $array): array
+    {
+        return array_intersect_key($array, get_class_vars(static::class));
+    }
+}
